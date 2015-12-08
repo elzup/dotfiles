250a1e9c906e40b2b1b6842ab4cbc2b642035ade
elzzup <guild0105@gmail.com>
2015-12-08 21:31:28 +0900

    reAdd vimperator files

---
+++ dotfiles/.vimperator/rc/js/localkey.rc.js
@@ -0,0 +1,10 @@
+liberator.globalVariables.localKeyMappings=
+[
+  [/twitter\.com/, [
+    ['<C-Enter>', '<Tab><CR>']
+  ]],
+  [/google\.com/, [
+    ['<C-j>', ':googleselect'],
+    ['<C-k>', ':googleselect back']
+  ]]
+];
---
+++ dotfiles/.vimperator/rc/js/toggle.rc.js
@@ -0,0 +1,4 @@
+liberator.globalVariables.toggler = {
+  navbar:  ["set gui=nonavigation", "set gui=navigation"],
+  bookbar:  ["set gui=nobookmarks", "set gui=bookmarks"],
+};
