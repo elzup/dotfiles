89f868df5d946711ad5d3fe3c13f7d747c5d1a0d
elzzup <guild0105@gmail.com>
2015-12-25 21:21:08 +0900

    Update zsh

--- dotfiles/.zsh/rc/envpath.zsh
+++ dotfiles/.zsh/rc/envpath.zsh
@@ -1,7 +1,7 @@
 . $ZSHRCHOME/envs/android.zsh
 # . $ZSHRCHOME/envs/vm.zsh
 # . $ZSHRCHOME/envs/cocos.zsh
-# . $ZSHRCHOME/envs/pyenv.zsh
+. $ZSHRCHOME/envs/pyenv.zsh
 # . $ZSHRCHOME/envs/phpenv.zsh
 . $ZSHRCHOME/envs/rbenv.zsh
 # . $ZSHRCHOME/envs/nvmenv.zsh
