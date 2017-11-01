177e463f1e963fb57e90ee958d7a9773b7cb88da
elzup <guild0105@gmail.com>
2017-11-02 08:35:09 +0900

    Clean shell

---
+++ dotfiles/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mv -f ~/Downloads/*.gif ~/gif/
+mv -f ~/Desktop/*.gif ~/gif/
+mv -f ~/Downloads/*.jpg ~/Desktop/
+mv -f ~/Downloads/*.JPG ~/Desktop/
+mv -f ~/Downloads/*.png ~/Desktop/
+mv -f ~/Downloads/*.docx ~/Desktop/
+mv -f ~/Downloads/*.pptx ~/Desktop/
+mv -f ~/Downloads/*.stl ~/Desktop/
+today=$(date "+%Y%m%d")
+mkdir -p ~/Documents/backup/desktop${today}
+mv ~/Desktop/* ~/Documents/backup/desktop${today}/
+mv ~/Desktop/.* ~/Documents/backup/desktop${today}/
+rm ~/Downloads/*
+rm ~/Downloads/.*
